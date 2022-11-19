1f8b55723645d507c7aa9c18521988b86f6fa427
Maxim Shastin <shastin_maksim@mail.ru>
2022-11-19 12:54:31 +0300

    feat: add section Tools

--- app/src/pages/home/Home.tsx
+++ app/src/pages/home/Home.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
 import BestTracker from './section BestTracker/BestTracker';
+import Tools from './section Tools/Tools';
+
 const Home = () => {
   return (
     <>
       <BestTracker />
+      <Tools />
     </>
   );
 };
