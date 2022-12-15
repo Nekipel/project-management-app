2111904c56d98ffb5821695f86e7824f1a85212e
User <a1@AN.local>
2022-12-15 22:32:36 +0300

    feat: add singin companents

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -5,7 +5,6 @@ import { ROUTES } from './constants/routes';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Context } from 'hooks/context';
 import Parse from 'parse';
-// import { Server } from './components/Server/Server';
 const PARSE_APPLICATION_ID = 'KnattpmQQW6mgrVGVCjGqHgFyLzJffEtMCa0v1jy';
 const PARSE_HOST_URL = 'https://parseapi.back4app.com/';
 const PARSE_JAVASCRIPT_KEY = '4dyP8U37mMaY3qkbd1ITyBTjasN6A9Yc6wsRRyoZ';
